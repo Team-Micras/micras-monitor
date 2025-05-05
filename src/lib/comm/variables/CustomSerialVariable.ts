f58d93f96641a6ba0a061e69cb6acfc7f12373a6
PedroDeSanti <phmartinsanti@gmail.com>
2025-05-05 23:23:24 +0200

    ✨ add CustomSerialVariable class for serializing and deserializing custom variables

---
+++ src/lib/comm/variables/CustomSerialVariable.ts
@@ -0,0 +1,64 @@
+import { ISerialVariable } from "./ISerialVariable";
+import { ISerializable } from "../ISerializable";
+
+/**
+ * Class for serializing and deserializing custom variables.
+ *
+ * @template T Type of the custom variable.
+ */
+export class CustomSerialVariable<T extends ISerializable>
+  implements ISerialVariable
+{
+  private valueRef: { value: T };
+  private name: string;
+  private readOnly: boolean;
+
+  /**
+   * Constructor for the CustomSerialVariable class.
+   *
+   * @param name Name of the variable.
+   * @param valueRef Reference object containing the value.
+   * @param readOnly True if the variable is read-only, false otherwise.
+   */
+  constructor(name: string, valueRef: { value: T }, readOnly: boolean) {
+    this.valueRef = valueRef;
+    this.name = name;
+    this.readOnly = readOnly;
+  }
+
+  /**
+   * Get the variable's name.
+   *
+   * @returns Name of the variable.
+   */
+  getName(): string {
+    return this.name;
+  }
+
+  /**
+   * Check if the variable is read-only.
+   *
+   * @returns True if the variable is read-only, false otherwise.
+   */
+  isReadOnly(): boolean {
+    return this.readOnly;
+  }
+
+  /**
+   * Serialize the variable.
+   *
+   * @returns Serialized data.
+   */
+  serialize(): Uint8Array {
+    return this.valueRef.value.serialize();
+  }
+
+  /**
+   * Deserialize the variable.
+   *
+   * @param serialData Pointer to the serialized data.
+   */
+  deserialize(serialData: Uint8Array): void {
+    this.valueRef.value.deserialize(serialData);
+  }
+}
