eaa7060e1d786ce7c52f947d978ccdcbf67e0ccd
PedroDeSanti <phmartinsanti@gmail.com>
2025-05-12 23:41:20 +0200

    ✨ Refactor App component to include CommunicationSelector and VariablesMonitor

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
 import './App.css'
 import { CommunicationProvider } from './contexts/CommunicationContext'
-import { BluetoothCommunication } from './components/BluetoothCommunication'
+import { CommunicationSelector } from './components/CommunicationSelector'
+import { VariablesMonitor } from './components/VariablesMonitor'
 
 function App() {
-
   return (
-      <CommunicationProvider>
-        <div className="app">
-          <header className="app-header">
-            <h1>Micras Monitor</h1>
-          </header>
-          <main>
-            <BluetoothCommunication />
-          </main>
-        </div>
-      </CommunicationProvider>
+    <CommunicationProvider>
+      <div className="app">
+        <header className="app-header">
+          <h1>Micras Monitor</h1>
+        </header>
+        <main>
+          <CommunicationSelector />
+          <VariablesMonitor />
+        </main>
+      </div>
+    </CommunicationProvider>
   )
 }
 
