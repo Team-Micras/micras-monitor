1f54d2eabadbd0f778bff35548fffca6432a1d86
PedroDeSanti <phmartinsanti@gmail.com>
2025-06-07 17:40:51 +0200

    💄 ✨ Include AppSidebar to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import { CommunicationProvider } from './contexts/CommunicationContext'
-import { CommunicationSelector } from './components/CommunicationSelector'
-import { VariablesMonitor } from './components/VariablesMonitor'
-import { customSerializableClasses } from './types'
+import { CommunicationProvider } from './contexts/CommunicationContext';
+import { customSerializableClasses } from './types/serializables';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
+import { CommunicationSelector } from './components/CommunicationSelector';
+import { VariablesMonitor } from './components/VariablesMonitor';
 
 function App() {
   return (
     <CommunicationProvider customSerializableClasses={customSerializableClasses}>
-      <div>
-        <header>
-          <h1>Micras Monitor</h1>
-        </header>
-        <main>
-          <CommunicationSelector />
-          <VariablesMonitor />
-        </main>
-      </div>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarTrigger />
+        <div>
+          <header>
+            <h1>Micras Monitor</h1>
+          </header>
+          <main>
+            <CommunicationSelector />
+            <VariablesMonitor />
+          </main>
+        </div>
+      </SidebarProvider>
     </CommunicationProvider>
-  )
+  );
 }
 
-export default App
+export default App;
